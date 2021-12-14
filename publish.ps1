be31627c4b256acd1b46b10494ff822a96342f03
Enner Pérez <ennerperez@gmail.com>
2021-12-14 11:35:31 -0500

    Publish Script

---
+++ publish.ps1
@@ -0,0 +1,17 @@
+cd src
+dotnet restore
+
+dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true -c Release --output ../publish/win-x64
+cd ../publish/win-x64
+7z a -tzip ../updater_win-x64.zip *
+cd ../../src
+
+dotnet publish -r osx-x64 -p:PublishSingleFile=true --self-contained true -c Release --output ../publish/osx-x64
+cd ../publish/osx-x64
+7z a -tzip ../updater_osx-x64.zip *
+cd ../../src
+
+dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true -c Release --output ../publish/linux-x64
+cd ../publish/linux-x64
+7z a -tzip ../updater_linux-x64.zip *
+cd ../../src
