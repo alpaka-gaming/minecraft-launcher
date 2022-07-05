1f27ad383822125aa99b296a9433f255ab807955
Enner Pérez <ennerperez@gmail.com>
2022-07-05 15:23:28 -0500

    Build & Pipeline

--- publish.ps1
+++ publish.ps1
@@ -1,17 +1,16 @@
 dotnet restore
-cd src
 
-dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true -c Release --output ../publish/win-x64
-cd ../publish/win-x64
+dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true -c Release --output publish/win-x64
+cd publish/win-x64
 7z a -tzip ../updater_win-x64.zip *
-cd ../../src
+cd ../..
 
-dotnet publish -r osx-x64 -p:PublishSingleFile=true --self-contained true -c Release --output ../publish/osx-x64
-cd ../publish/osx-x64
+dotnet publish -r osx-x64 -p:PublishSingleFile=true --self-contained true -c Release --output publish/osx-x64
+cd publish/osx-x64
 7z a -tzip ../updater_osx-x64.zip *
-cd ../../src
+cd ../..
 
-dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true -c Release --output ../publish/linux-x64
-cd ../publish/linux-x64
+dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true -c Release --output publish/linux-x64
+cd publish/linux-x64
 7z a -tzip ../updater_linux-x64.zip *
-cd ../../src
+cd ../..
